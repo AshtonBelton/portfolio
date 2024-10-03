7ab19a811b7778d4681e8f560cff681facff04b9
Ashton Belton <ashbelton24@gmail.com>
2024-10-03 14:58:15 -0600

    Start of the project, currently working on introduction to my page

---
+++ my-portfolio/src/intro.jsx
@@ -0,0 +1,6 @@
+<div className="intro-section">
+    <h1>
+        Hi, I'm a <span className="dynamic-text"></span>
+    </h1>
+    <p>Part-time student with a passion for web development.</p>
+</div>
